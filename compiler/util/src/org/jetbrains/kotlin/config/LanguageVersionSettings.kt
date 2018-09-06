8fc095c6156857ea8e4c388486d892d014e93467
Denis Zharkov <denis.zharkov@jetbrains.com>
2018-09-06 19:19:40 +0300

    Add language feature for extended main convention
    - suspend
    - parameterless
    
     #KT-17679 Fixed
     #KT-26574 Fixed

--- compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
+++ compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
@@ -88,6 +88,7 @@ enum class LanguageFeature(
     SoundSmartcastFromLoopConditionForLoopAssignedVariables(KOTLIN_1_3, kind = BUG_FIX),
     DslMarkerOnFunctionTypeReceiver(KOTLIN_1_4, kind = BUG_FIX),
     ProhibitErroneousExpressionsInAnnotationsWithUseSiteTargets(KOTLIN_1_3, kind = BUG_FIX),
+    ExtendedMainConvention(KOTLIN_1_3),
 
     RestrictReturnStatementTarget(KOTLIN_1_4, kind = BUG_FIX),
     ProperIeee754Comparisons(sinceVersion = null, defaultState = State.DISABLED, kind = BUG_FIX),
